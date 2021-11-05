26022d1efe8a91000b5b84bcb5cac2de736bd447
Rohit <L00163278@student.lyit.ie>
2021-11-05 10:03:54 +0000

    Git Hub Demo

---
+++ eclipse-workspace/mvnSimpleProj/src/main/java/mvnSimpleProj/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mvnSimpleProj;
+
+/**
+ * @author Ether
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello LYIT");
+
+	}
+
+}
